e2ae8b6687fae7d33b8263c1baaebbe8c8a2efdf
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-11-26 21:51:20 +0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const checkEditMode = document.querySelector('.bx-panel-toggle-on') ?? null;
+
+  /**
+   * Подключение ScrollTrigger
+   */
+  gsap.registerPlugin(ScrollTrigger, SplitText);
+
+  /**
+   * Инициализация Lenis
+   */
+  const lenis = new Lenis({
+    anchors: {
+      offset: -60,
+    },
+  });
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+});
